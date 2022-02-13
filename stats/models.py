95baba42615ad438945dc7b1b3caa98a58193733
Varun Tiwari <varun.2011t@gmail.com>
2022-02-14 02:00:37 +0530

    Updated About Content

--- stats/models.py
+++ stats/models.py
@@ -10,7 +10,7 @@ class AboutApp(models.Model):
 
 
 class Developer(models.Model):
-    order = models.IntegerField(primary_key=True)
+    order = models.IntegerField(default=0)
     name = models.CharField(max_length=100, default="")
     tags = models.CharField(max_length=400, default="")
     linkedin = models.CharField(max_length=400, default="")
